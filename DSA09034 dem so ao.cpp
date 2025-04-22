3494614c7b1ac9b3b6655456850e98ffeea426aa
DungNguyenCoder <dungnickchinh1909@gmail.com>
2025-04-22 11:43:36 +0700

    DSA

---
+++ DSA09034 dem so ao.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define faster() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+#define ll long long
+#define endl "\n"
+
+const int mx = 1e5;
+const int mod = 1e9+7;  
+#define TEST 0
+
+char a[505][505];
+
+int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[8] = {-1, 0, 1, -1, 1, -1, 0 ,1};
+
+void loang(int i, int j, int n, int m) {
+    a[i][j] = '.';
+    for (int k = 0; k < 8; k++) {
+        int i1 = i + dx[k];
+        int j1 = j + dy[k];
+        if(i1 >= 0 && i1 < n && j1 >= 0 && j1 < m && a[i1][j1] == 'W') {
+            loang(i1, j1, n, m);
+        }
+    }
+}
+
+inline void solution() {
+    int n, m;
+    cin >> n >> m;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if(a[i][j] == 'W') {
+                loang(i, j, n, m);
+                ++cnt;
+            }
+        }
+    }
+    cout << cnt << endl;
+}
+
+int main() {
+    faster();
+    int t;
+    if(TEST) {
+        cin >> t;
+        cin.ignore();
+    }
+    else        t = 1;
+    while(t--) {
+        solution();
+    }
+}
